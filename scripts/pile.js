2149c07bb06ec4605050309e7baeb8dd5b3d39b7
MaoWenChao <mwc@chinasap.cn>
2017-12-27 20:41:23 +0800

    url error

--- scripts/pile.js
+++ scripts/pile.js
@@ -1771,7 +1771,7 @@
             loadCounter += 1;
 
             ajax({
-                url: '../data/sample.json',
+                url: './../data/sample.json',
                 success: loadDataSuccess,
                 fail: function () {
                     loadDataFail();
